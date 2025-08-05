bea9d4d0824bfd6f98ba0e898395988ad98d53dc
kamalabot <kamaljp@gmail.com>
2025-08-05 16:31:11 +0530

    commiting scripts

---
+++ basics_exploring/operators.py
@@ -0,0 +1,10 @@
+a = 10
+b = 3
+
+print(a + b)   # 13 (Addition)
+# print(a - b)   # 7 (Subtraction)
+# print(a * b)   # 30 (Multiplication)
+# print(a / b)   # 3.333... (Division)
+# print(a % b)   # 1 (Modulus – reminder)
+# print(a ** b)  # 1000 (Power – a raised to b)
+# print(a // b)  # 3 (Floor Division – no decimals)
---
+++ basics_exploring/variables.py
@@ -0,0 +1,25 @@
+# Variable declarations
+name = "Kamal"          # string
+age = 30                # integer
+height = 5.8            # float
+is_student = True       # boolean
+skills = ["Python", "AI", "Math"]  # list
+
+# Displaying values
+print(name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student?", is_student)
+print("Skills:", skills)
+
+x = 10
+print(type(x))   # <class 'int'>
+
+y = "Hello"
+print(type(y))   # <class 'str'>
+
+juice = "orange"
+print(juice)  # orange
+
+juice = "mango"
+print(juice)  # mango
